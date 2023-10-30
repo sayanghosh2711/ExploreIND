0331b22e569b84afec98b4c54d26ffc02fdd43b3
sayanghosh2711 <42648746+sayanghosh2711@users.noreply.github.com>
2023-10-30 16:59:54 +0530

    d

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("search-button").addEventListener("click", function () {
+    // This is where you would handle the search functionality.
+    // You can use AJAX or Fetch to request destination details from the server.
+    // Once you have the details, populate the #destination-details section with the results.
+});
