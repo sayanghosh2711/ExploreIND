7890bc1b9a9d431e257d1fa440f4be25402d6310
sayanghosh2711 <42648746+sayanghosh2711@users.noreply.github.com>
2023-10-30 19:56:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-document.getElementById("search-button").addEventListener("click", function () {
-    // This is where you would handle the search functionality.
-    // You can use AJAX or Fetch to request destination details from the server.
-    // Once you have the details, populate the #destination-details section with the results.
-});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
